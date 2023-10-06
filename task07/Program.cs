f839436fbba84ae63009988e02f9df4de1e54e77
Angy <rosa_02@mail.ru>
2023-10-06 04:30:46 +0300

    task 07

---
+++ task07/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая принимает на взод трехзначное число и на выходе показывает последнюю цифру этого числа
+
+Console.WriteLine("Введите трехзначное целое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number < 0) number *= -1;
+if (number>= 100&& number <= 999) 
+{
+int lastDiggit = number%10;
+Console.WriteLine($"Последняя цифра числа: {lastDiggit}");
+}
+
+else Console.WriteLine ("Wrong number");
